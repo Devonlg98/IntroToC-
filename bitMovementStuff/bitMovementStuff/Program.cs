350a15d095ba0b1eff57f773e854fd1a2a920e2e
Devon Gregory <Devonlg98@gmail.com>
2019-09-25 12:52:14 -0700

    Classwork done and clean up of files

---
+++ bitMovementStuff/bitMovementStuff/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bitMovementStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0;
+            int size = 1;
+            int[] binaryArray = new int[size];
+            Int32.TryParse(Console.ReadLine(), out input);
+            //do
+            //{
+            //    size++;
+
+            //    if (input %2 == 0)
+            //    {
+            //        binaryArray[size-1] = 0;
+            //    }
+            //    else
+            //    {
+            //        binaryArray[size-1] = 1;
+            //    }
+            //    input /= 2;
+            //} while (input > 0);
+            for(int i = 0; input > 0; i++)
+            {
+                if (input % 2 == 0)
+                {
+                    binaryArray[i] = 0;
+                }
+                else
+                {
+                    binaryArray[i] = 1;
+                }
+                input /= 2;
+            }
+            for (int i = 0; i<size; i++)
+            {
+                Console.Write(binaryArray[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
