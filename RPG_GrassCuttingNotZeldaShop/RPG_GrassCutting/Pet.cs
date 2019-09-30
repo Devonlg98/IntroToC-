557d485ad60da2553eaa6229376788a8ec4a6773
Devon Gregory <Devonlg98@gmail.com>
2019-09-30 16:57:12 -0700

    Clean Up

---
+++ RPG_GrassCuttingNotZeldaShop/RPG_GrassCutting/Pet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_GrassCutting
+{
+
+    enum PetType { Wolf, Fox };
+
+    class Pet
+    {
+
+        public PetType PType;
+        public void HeadPat()
+        {
+            Console.WriteLine($"You pet your {PType}");
+        }
+    }
+}
