d841db786640caedf7cdff1cea6f9ea0f3fd0ba0
Devon Gregory <Devonlg98@gmail.com>
2019-10-14 17:54:41 -0700

    ugh

---
+++ raygamecsharp-master/raygamecsharp-master/ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using Raylib;
+using rl = Raylib.Raylib;
+
+namespace ConsoleApp1
+{
+    public class SceneObject
+    {
+        protected SceneObject parent = null;
+        protected List<SceneObject> children = new List<SceneObject>();
+        public SceneObject Parent
+        {
+            get { return parent; }
+        }
+        public SceneObject()
+        {
+        }
+        public int GetChildCount()
+        {
+            return children.Count;
+        }
+        public SceneObject GetChild(int index)
+        {
+            return children[index];
+        }
+    }
+    static class Program
+    {
+        public static int Main()
+        {
+
+            // Initialization
+            //--------------------------------------------------------------------------------------
+            int screenWidth = 800;
+            int screenHeight = 450;
+
+            rl.InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+            rl.SetTargetFPS(60);
+            //--------------------------------------------------------------------------------------
+
+            // Main game loop
+            while (!rl.WindowShouldClose())    // Detect window close button or ESC key
+            {
+                // Update
+                //----------------------------------------------------------------------------------
+                // TODO: Update your variables here
+                //----------------------------------------------------------------------------------
+
+                // Draw
+                //----------------------------------------------------------------------------------
+                rl.BeginDrawing();
+
+                rl.ClearBackground(Color.RAYWHITE);
+
+                rl.DrawText("Congrats! You created your first window!", 190, 200, 20, Color.LIGHTGRAY);
+
+                rl.EndDrawing();
+                //----------------------------------------------------------------------------------
+            }
+
+            // De-Initialization
+            //--------------------------------------------------------------------------------------
+            rl.CloseWindow();        // Close window and OpenGL context
+                                     //--------------------------------------------------------------------------------------
+
+            return 0;
+        }
+    }
+}
