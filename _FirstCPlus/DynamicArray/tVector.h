f366c2e0a6f2a414611f48cf47215f0710ad3d24
Devon Gregory <Devonlg98@gmail.com>
2020-01-14 16:24:48 -0800

    finally snow

--- _FirstCPlus/DynamicArray/tVector.h
+++ _FirstCPlus/DynamicArray/tVector.h
@@ -27,11 +27,66 @@ public:
 	T &at(size_t index);                // returns the element at the given index
 
 	size_t size() const;                // returns current number of elements
-	size_t capacity() const;            // returns maximum number of elements we can store
+	size_t capacity() const;      		// returns maximum number of elements we can store
+	
+	T *data()
+	{
+		return *arr;
+	}
+
+	void reserve(size_t newCapacity)
+	{
+		tempArr = new T[vec.arrCapacity];
+		for (int i = 0; i < arrSize; i++)
+		{
+			temp[i] = arr[i];
+		}
+		arrCapacity = newCapacity;
+		delete[] arr;
+		arr[i] = temp[i];
+		delete[] temp;
+	}
+
+	// adds an element to the end of the vector which is value
+	void push_back(const T &value)
+	{
+		if (arrSize >= arrCapacity)
+		{
+			reserve(arrSize * 2);
+		}
+		arr[arrSize] = value;
+		// Adding new element, so add to variable of size for future use
+		arrSize++;
+
+	}
+
+	// destroys and removes the last element of the vector
+	void pop_back()
+	{
+		arr[arrSize - 1] = nullptr;
+	}
+
+	// returns the element at the given index
+	// check index valid number under arr size
+	T &at(size_t index)
+	{
+		return 
+	}
+	size_t size() const
+	{
+
+	}
+	size_t capacity() const
+	{
+
+	}
+	
+
 };
 
 
 template <typename T>
+
 tVector<T>::tVector()
 {
 	
@@ -45,5 +100,33 @@ tVector<T>::~tVector()
 
 }
 
+ template <typename T>
+tVector<T>::tVector(const tVector &vec)
+{
+	arrSize = vec.arrSize;
+	arrCapacity = vec.arrCapacity;
+	arr = new T[vec.arrCapacity];
+	for (int i = 0; i < arrSize; i++)
+	{
+		arr[i] = vec.arr[i];
+	}
+}
 
+template <typename T>
+tVector<T>& tVector<T>::operator=(const tVector<T> &vec)
+{
+	this->arrSize = vec.arrSize;
+	this->arrCapacity = vec.arrCapacity;
+
+	delete[] arr;
+
+	arr = new T[vec.arrCapacity];
+	for (int i = 0; i < arrSize; i++)
+	{
+		arr[i] = vec.arr[i];
+	}
+}
+
+
+//Return type-> class -> function name
 
